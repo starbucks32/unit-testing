1cee0db4067d9cc75e77f021fe54cfa21072d8ca
Alex <40953226+starbucks32@users.noreply.github.com>
2018-07-08 15:06:24 +0100

    Add other arithmetic functions.

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ function subtract(a, b){
   return a - b;
 }
 
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
 module.exports = {
   sum: sum,
-  subtract: subtract
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide
 }
